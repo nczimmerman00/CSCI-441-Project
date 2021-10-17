fae8b158328b05ea1ab9cd7378c9c4a9c1833246
nczimmerman00 <89539108+nczimmerman00@users.noreply.github.com>
2021-10-17 18:55:51 -0500

    Add files via upload

---
+++ RecordsManagement.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Remote_Garden_Control_Center
+{
+    class RecordKeeper
+    {
+        //TODO
+    }
+
+    class RecordRetriever
+    {
+        //TODO
+    }
+}
